fd5ee40c7a3a057bcf1e747091269845a520da63
pvictor <perrier.victor@gmail.com>
2020-05-05 16:22:03 +0200

    added example data

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Cities around the world.
+#'
+#' Dataset with hierarchical levels for cities.
+#'
+#' @format A data frame with 11 rows and 3 variables:
+#' \describe{
+#'   \item{continent}{Continent}
+#'   \item{country}{Country}
+#'   \item{city}{city}
+#' }
+#' @source The world
+"cities"
---
+++ data-raw/cities.R
@@ -0,0 +1,19 @@
+## code to prepare `cities` dataset goes here
+
+library(stringi)
+
+cities <- data.frame(
+  continent = c("America", "America", "America", "Africa",
+                "Africa", "Africa", "Africa", "Africa",
+                "Europe", "Europe", "Europe"),
+  country = c("Canada", "Canada", "USA", "Tunisia", "Tunisia",
+              "Tunisia", "Algeria", "Algeria", "Italy", "Germany", "Spain"),
+  city = c("Trois-Rivières", "Québec", "San Francisco", "Tunis",
+           "Monastir", "Sousse", "Alger", "Oran", "Rome", "Berlin", "Madrid"),
+  stringsAsFactors = FALSE
+)
+
+cities$city <- stringi::stri_trans_general(str = cities$city, id = "ASCII-Latin")
+
+
+usethis::use_data(cities, overwrite = TRUE)
